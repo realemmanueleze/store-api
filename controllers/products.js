73493b6f0c019753ea68f7e25ad94be2a71486a2
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2024-04-25 21:32:39 +0100

    sort, select, limit, pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,9 @@
 const Product = require("../models/products");
 
 const getAllProducts = async (req, res, next) => {
-  const { featured, company, name } = req.query;
-  console.log(featured);
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured;
   }
@@ -13,13 +13,31 @@ const getAllProducts = async (req, res, next) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  const products = await Product.find(queryObject);
-  res.status(200).json({ msg: products });
+
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  }
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result.skip(skip).limit(limit);
+
+  const products = await result;
+
+  res.status(200).json({ nbHits: products.length, msg: products });
 };
 
 const getAllProductsStatic = async (req, res, next) => {
-  const products = await Product.find({});
-  res.status(200).json({ msg: products });
+  const products = await Product.find({}).sort("name -price");
+  res.status(200).json({ nbHits: products.length, msg: products });
 };
 
 module.exports = {
