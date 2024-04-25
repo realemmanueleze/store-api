3b4bb5a9b7400775d634910fa45be7221b68b799
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2024-04-25 16:33:35 +0100

    implement getAllProducts endpoint with query params (company, name, featured)

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,25 @@
-const getAllProducts = (req, res, next) => {
-  res.status(200).json({ msg: "All products" });
+const Product = require("../models/products");
+
+const getAllProducts = async (req, res, next) => {
+  const { featured, company, name } = req.query;
+  console.log(featured);
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ msg: products });
 };
-const getAllProductsStatic = (req, res, next) => {
-  res.status(200).json({ msg: "All static products" });
+
+const getAllProductsStatic = async (req, res, next) => {
+  const products = await Product.find({});
+  res.status(200).json({ msg: products });
 };
 
 module.exports = {
